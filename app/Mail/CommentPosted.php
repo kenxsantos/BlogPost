22b6deb1da26ea940f1d246ff70385a8a2f28eec
Ken Santos <kenxsantos@gmail.com>
2024-03-25 22:37:11 +0800

    Day 19

---
+++ app/Mail/CommentPosted.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CommentPosted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Comment Posted',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        $subject = "Commented was posted on your {$this->comment->commentable->title} blog post";
+        return (new Content(
+            $subject))->view('emails.posts.commented');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
